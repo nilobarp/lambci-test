39f37afe1a3ead9a9202e4889bc9b3846244034f
Nilotpal Barpujari <nilotpal@tradeledger.io>
2018-04-30 08:42:58 +1000

    add files

---
+++ tests/index.test.ts
@@ -0,0 +1,11 @@
+import { handler } from "../index";
+describe("Simple test", () => {
+  it("works", () => {
+    const callbackFn = jest.fn();
+    handler({}, {}, callbackFn);
+    expect(callbackFn).toHaveBeenCalledWith(undefined, {
+      body: "Hello world",
+      statusCode: 200
+    });
+  });
+});
